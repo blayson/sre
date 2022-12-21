1c4c2761a72a7d90b0a5992bd135fbec572a5724
Andrii But <abut@redhat.com>
2022-12-22 00:17:21 +0100

    fix2

--- app/__main__.py
+++ app/__main__.py
@@ -1,11 +1,11 @@
 import uvicorn
 
 from .settings import settings
-
-uvicorn.run(
-    "app.main:app",
-    host=settings.server_host,
-    port=settings.server_port,
-    reload=True,
-    log_level="debug",
-)
+#
+# uvicorn.run(
+#     "app.main:app",
+#     host=settings.server_host,
+#     port=settings.server_port,
+#     reload=True,
+#     log_level="debug",
+# )
